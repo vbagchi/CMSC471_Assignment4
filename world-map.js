c41ff9dd1262025beb44c90bee5865ceddcabe24
Vasundhara <vasundhara.bagchi@gmail.com>
2023-04-28 00:42:28 -0400

    deleted extra colorscale stuff

--- world-map.js
+++ world-map.js
@@ -8,45 +8,18 @@ const SVG_HEIGHT = 600;
 // Create a function that a population number
 // to a shade of blue for country fills.
 
-/*
-const color = d3
-  .scaleThreshold()
-  .domain([
-    10000,
-    100000,
-    500000,
-    1000000,
-    5000000,
-    10000000,
-    50000000,
-    100000000,
-    500000000,
-    1500000000
-  ])
-  .range([
-    'rgb(247,251,255)',
-    'rgb(222,235,247)',
-    'rgb(198,219,239)',
-    'rgb(158,202,225)',
-    'rgb(107,174,214)',
-    'rgb(66,146,198)',
-    'rgb(33,113,181)',
-    'rgb(8,81,156)',
-    'rgb(8,48,107)',
-    'rgb(3,19,43)'
-  ]);
-*/
-  const popcolor = d3.scaleSequential(d3.interpolateViridis)
+const popcolor = d3.scaleSequential(d3.interpolateViridis)
   .domain([0, 1500000000]);
 
-  const gdpcolor = d3.scaleSequential(d3.interpolateViridis)
+const gdpcolor = d3.scaleSequential(d3.interpolateViridis)
   .domain([0, 22000]);
 
 // Create a function to format numbers with commas.
-const format = d3.format(',');
+
+//const format = d3.format(',');
 const tooltip = d3.select('.tooltip');
-const tooltipCountry = tooltip.select('.country');
-const tooltipPopulation = tooltip.select('.population');
+//const tooltipCountry = tooltip.select('.country');
+//const tooltipPopulation = tooltip.select('.population');
 
 function hideTooltip() {
   tooltip.style('opacity', 0);
